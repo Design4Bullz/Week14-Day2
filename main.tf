b3a8c6259e6efab935838d5ba799aa962dbe6f41
Design4Bullz <73964355+Design4Bullz@users.noreply.github.com>
2024-07-02 17:12:05 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,95 @@ terraform {
     }
   }
 }
+
+resource "aws_security_group" "gha-tf-ans-demo-control" {
+  name        = "gha-tf-ans-demo-control"
+  description = "Security Group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "gha-tf-ans-demo-targets" {
+  name        = "gha-tf-ans-demo-targets"
+  description = "Security Group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "ssh_private_key_path" {
+  description = "Path to SSH private key"
+  default     = "~/terraform-aws/aws-key.pem"
+}
+
+resource "aws_instance" "master_node" {
+  ami                    = "ami-0aa7d40eeae50c9a9"
+  instance_type          = "t2.micro"
+  key_name               = "aws-key"
+  vpc_security_group_ids = [aws_security_group.gha-tf-ans-demo-control.id]
+
+  tags = {
+    Name = "master_node"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'hello world!'",
+      "pwd",
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install ansible2 -y",
+    ]
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file(var.ssh_private_key_path)
+    }
+  }
+}
+
